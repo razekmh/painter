5a1c9529c18a46eb48daf65d90aa4797c0e79ede
razekmh <razekmh@gmail.com>
2023-12-16 19:09:15 +0000

    make square shake

--- pygame_display_file.py
+++ pygame_display_file.py
@@ -6,10 +6,17 @@ pygame.init()
 
 # Set up the HyperPixel display
 width, height = 800, 480
-screen = pygame.display.set_mode((width, height), pygame.FULLSCREEN)
+screen = pygame.display.set_mode((width, height))
 
 # Load the image from the file
 image = pygame.image.load("output_image.png")
+
+# Initial position of the square
+square_x, square_y = 100, 100
+
+# Speed and direction of the square movement
+speed = 5
+direction = (1, 1)  # (1, 1) means moving diagonally down and to the right
 
 # Main loop
 while True:
@@ -18,8 +25,22 @@ while True:
             pygame.quit()
             sys.exit()
 
+    # Update the position of the square
+    square_x += speed * direction[0]
+    square_y += speed * direction[1]
+
+    # Bounce off the screen edges (optional)
+    if square_x <= 0 or square_x + image.get_width() >= width:
+        direction = (-direction[0], direction[1])
+    if square_y <= 0 or square_y + image.get_height() >= height:
+        direction = (direction[0], -direction[1])
+
     # Draw the image on the screen
-    screen.blit(image, (0, 0))
+    screen.fill((255, 255, 255))  # Clear the screen with a white background
+    screen.blit(image, (square_x, square_y))
 
     # Update the display
     pygame.display.flip()
+
+    # Add a small delay to control the frame rate
+    pygame.time.delay(10)
