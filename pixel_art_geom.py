4793cf27dae24ded2ea9a140c3d8f96db45c73d5
razekmh <razekmh@gmail.com>
2023-12-17 23:47:07 +0000

    add print square index

--- pixel_art_geom.py
+++ pixel_art_geom.py
@@ -9,7 +9,7 @@ sqr_top = 10
 sqr_width = 10
 sqr_height = 10
 
-size_of_cell = 5
+size_of_cell = 20
 count_of_rows = int(screen_width / size_of_cell)
 count_of_cols = int(screen_height / size_of_cell)
 sqr_width = sqr_height = size_of_cell
@@ -33,24 +33,41 @@ def create_dict_of_squares():
     for i in range(0, count_of_rows):
         for j in range(0, count_of_cols):
             white_colour = (255, 255, 255)
-            dict_of_squares[square_index] = (
-                pygame.Rect(
+            dict_of_squares[square_index] = {
+                "location": pygame.Rect(
                     (i * size_of_cell) + badding,
                     (j * size_of_cell) + badding,
                     sqr_width - badding,
                     sqr_height - badding,
                 ),
-                white_colour,
-            )
+                "colour": white_colour,
+            }
+            square_index += 1
+    print(square_index)
+    return dict_of_squares
 
-            square_index = +1
 
+def write_numbers_on_squares(dict_of_squares):
+    font = pygame.font.SysFont("Arial", 10)
+    for square_index, square_info in dict_of_squares.items():
+        screen.blit(
+            font.render(str(int(square_index)), True, (255, 0, 0)),
+            square_info["location"],
+        )
+
+
+def colour_specific_squares(dict_of_squares):
+    for square_index in list_of_colouring:
+        dict_of_squares[square_index] = {
+            "location": dict_of_squares[square_index]["location"],
+            "colour": (255, 0, 0),
+        }
     return dict_of_squares
 
 
 def draw_squares_from_dict(dict_of_squares):
     for square_index, square_info in dict_of_squares.items():
-        pass
+        pygame.draw.rect(screen, square_info["colour"], square_info["location"])
 
 
 def draw_grid_of_squares():
@@ -78,7 +95,10 @@ while running:
             break
     screen.fill((0, 0, 0))
     # draw_two_rects()
-    draw_grid_of_squares()
+    # draw_grid_of_squares()
+    dict_of_squares = create_dict_of_squares()
+    draw_squares_from_dict(dict_of_squares)
+    write_numbers_on_squares(dict_of_squares)
     pygame.display.update()
 
 pygame.quit()
